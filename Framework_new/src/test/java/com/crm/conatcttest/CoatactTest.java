28bb8a1e2e83f50f76c4aa1bf0dd35416ce83a75
Deepak <Deepak@Ty-Deepak-Tr>
2021-02-02 16:17:14 +0530

    push to local

---
+++ Framework_new/src/test/java/com/crm/conatcttest/CoatactTest.java
@@ -0,0 +1,12 @@
+package com.crm.conatcttest;
+
+import org.testng.annotations.Test;
+
+public class CoatactTest {
+	
+	@Test
+	public void createCotactTest() {
+		System.out.println("execute create Contact");
+	}
+
+}
